5544610e54f3d3d16c1626364372cf9f742bbcb3
Niusha Yaghini <niusha.yaghini@gmail.com>
2026-09-06 13:29:56 +0330

    Tic-Tac-Toe Game (Game Environment Only)

--- 03-adversarial-search-and-game-ai/src/games.py
+++ 03-adversarial-search-and-game-ai/src/games.py
@@ -10,7 +10,136 @@
 
 
 # ============================================================
-# Tic-Tac-Toe
+# Total Game Structure
 # ============================================================
+from abc import ABC, abstractmethod
 
 
+class Game(ABC):
+
+    @abstractmethod
+    def initial_state(self):
+        pass
+
+    @abstractmethod
+    def actions(self, state):
+        pass
+
+    @abstractmethod
+    def result(self, state, action):
+        pass
+
+    @abstractmethod
+    def terminal_test(self, state):
+        pass
+
+    @abstractmethod
+    def utility(self, state):
+        pass
+    
+    
+# ============================================================
+# Tic-Tac-Toe Game
+# ============================================================
+class TicTacToe(Game):
+    def __init__(self):
+        self.empty = " "
+
+        self.winning_lines = [
+            (0,1,2),
+            (3,4,5),
+            (6,7,8),
+
+            (0,3,6),
+            (1,4,7),
+            (2,5,8),
+
+            (0,4,8),
+            (2,4,6),
+        ]
+        
+    def initial_state(self):
+        board = (
+            self.empty,
+        ) * 9
+        
+        return (
+            board,
+            # This only determines which player starts the game.
+            "X",
+        )
+        
+    def actions(
+        self,
+        state,
+    ):
+
+        board, _ = state
+
+        return [
+            index
+            for index, value in enumerate(board)
+            if value == self.empty
+        ]
+        
+    def result(
+        self,
+        state,
+        action,
+    ):
+        board, player = state
+        new_board = list(board)
+        new_board[action] = player
+
+        next_player = (
+            "O"
+            if player == "X"
+            else "X"
+        )
+
+        return (
+            tuple(new_board),
+            next_player,
+        )
+        
+    def winner(self, board):
+        for line in self.winning_lines:
+            a,b,c = line
+
+            if (
+                board[a] != self.empty
+                and
+                board[a] == board[b]
+                and
+                board[b] == board[c]
+            ):
+                return board[a]
+
+        return None
+    
+    def terminal_test(
+        self,
+        state,
+    ):
+        board, _ = state
+
+        return (
+            self.winner(board) is not None
+            or
+            self.empty not in board
+        )
+        
+    def utility(
+        self,
+        state,
+    ):
+        board, _ = state
+        winner = self.winner(board)
+
+        if winner == "X":
+            return 1
+
+        if winner == "O":
+            return -1
+
+        return 0
