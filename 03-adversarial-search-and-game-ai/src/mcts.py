b5eef0822f25d84d43c73bffdd44236ba3748f2c
Niusha Yaghini <niusha.yaghini@gmail.com>
2026-09-07 13:41:21 +0330

    Step 7.2: Expansion + Step 7.3: Simulation / Rollout

--- 03-adversarial-search-and-game-ai/src/mcts.py
+++ 03-adversarial-search-and-game-ai/src/mcts.py
@@ -1,4 +1,6 @@
 import math
+import random
+
 
 # MCTSNode:
     # select()
@@ -43,6 +45,66 @@ def select(node):
             key=uct_value
         )
     return node
+
+def expand(node):
+    if not node.untried_actions:
+        return node
+
+    action = node.untried_actions.pop()
+
+    new_state = node.game.result(
+        node.state,
+        action,
+    )
+
+    child = MCTSNode(
+        node.game,
+        new_state,
+        parent=node,
+        action=action,
+    )
+
+    node.children.append(
+        child
+    )
+
+    return child
+
+def simulate(
+    node,
+):
+    game = node.game
+    state = node.state
+
+    while not game.terminal_test(state):
+        actions = list(
+            game.actions(state)
+        )
+        action = random.choice(
+            actions
+        )
+        state = game.result(
+            state,
+            action,
+        )
+    # return game.utility(
+    #     state
+    # )
+    return mcts_reward(
+        game,
+        state,
+    )
+
+def mcts_reward(
+    game,
+    state,
+):
+    utility = game.utility(state)
+    if utility > 0:
+        return 1
+    if utility < 0:
+        return -1
+    return 0
 
 class MCTSNode:
     def __init__(
